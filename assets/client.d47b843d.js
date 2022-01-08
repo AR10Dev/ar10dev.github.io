3fccc6fca11b1c7e6039fca1cb080d48ef54af5b
AR10Dev <>
2022-01-08 17:15:53 +0000

    Deploying to gh-pages from @ AR10Dev/ar10dev.github.io@6494cb82afbee0ae7efea593a0ae84bf1e708e60 🚀

---
+++ assets/client.d47b843d.js
@@ -0,0 +1,1 @@
+import{h as o,c}from"./vendor.c53b6139.js";var m=r=>(n,a,t)=>{let e;t!=null&&(e=document.createElement("astro-fragment"),e.innerHTML=t),o(()=>c(n,{...a,children:e}),r)};export{m as default};
