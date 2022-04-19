38ce127dba4167ef1f2d5704b68f3b9aaef29037
AR10 <avaabrazzaq10@gmail.com>
2022-04-19 23:33:27 +0200

    Disable ESLint roles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { registerSW } from 'virtual:pwa-register'
 
 const updateSW = registerSW({
